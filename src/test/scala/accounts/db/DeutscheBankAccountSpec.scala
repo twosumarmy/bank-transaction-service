8dbc8911a6ff6f0c3ec6a8710a9045e18433f0e6
abteilung6 <moeller.tilman@gmail.com>
2022-07-15 14:29:20 +0200

    feat: deutsche bank account

---
+++ src/test/scala/accounts/db/DeutscheBankAccountSpec.scala
@@ -0,0 +1,85 @@
+package accounts.db
+
+import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import java.time.LocalDate
+import scala.io.Source
+
+class DeutscheBankAccountSpec extends AnyWordSpec with Matchers with BeforeAndAfterAll with FailFastCirceSupport {
+
+  import io.circe.parser._
+  import DeutscheBankAccount.{ BankAccount, Transaction }
+  import DeutscheBankAccount.Codecs._
+  import DeutscheBankAccount.Response._
+
+  "transactionDecoder" should {
+    val expectedTransaction = Transaction(
+      "2rVXBkxUMbLWfsKBr-tdy9jPe7BDme3ISVkHwTBTFFeYoJyPxRo8lIIKr5rYei5xJVrm1HffjxU99ksk9jHwsnfo8fSBJvSg0J8GaoGGhpKYLBTt43kPjJ-aLyztHuWa",
+      -200.00,
+      "DE00500700100200000027",
+      None,
+      "Deutsche Bank",
+      "EUR",
+      "Barauszahlung, Schule",
+      LocalDate.parse("2021-11-24"),
+      "123",
+      "D001",
+      "CCRD",
+      "CWDL",
+      "ABMX0355443",
+      "DE0111100004544221",
+      "E2E - Reference",
+      "ZKLE 911/XC:121600 ABC",
+      LocalDate.parse("2018-04-23")
+    )
+
+    "decode to object Transaction" in {
+      val text: String = Source.fromResource("db/transaction.json").mkString
+      decode[Transaction](text) match {
+        case Right(transaction) => assert(transaction == expectedTransaction)
+        case Left(error)        => assert(false, error.toString)
+      }
+    }
+  }
+
+  "bankAccountDecoder" should {
+    val expectedBankAccount = BankAccount(
+      "DE00500700100200000027",
+      "EUR",
+      "DEUTDEFFXXX",
+      "CURRENT_ACCOUNT",
+      100.95,
+      "Persönliches Konto"
+    )
+
+    "decode to object BankAccount" in {
+      val text: String = Source.fromResource("db/bankAccount.json").mkString
+      decode[BankAccount](text) match {
+        case Right(bankAccount) => assert(bankAccount == expectedBankAccount)
+        case Left(error)        => assert(false, error.toString)
+      }
+    }
+  }
+
+  "getTransactionsResponseDecoder" should {
+    "decode to object GetTransactionsResponse" in {
+      val text: String = Source.fromResource("db/getTransactionsResponse.json").mkString
+      decode[GetTransactionsResponse](text) match {
+        case Right(getTransactionsResponse) => assert(getTransactionsResponse.transactions.length == 10)
+        case Left(error)                    => assert(false, error.toString)
+      }
+    }
+  }
+
+  "getBankAccountsResponseDecoder" should {
+    "decode to object GetBankAccountsResponse" in {
+      val text: String = Source.fromResource("db/getBankAccountsResponse.json").mkString
+      decode[GetBankAccountsResponse](text) match {
+        case Right(getBankAccountsResponse) => assert(getBankAccountsResponse.accounts.length == 1)
+        case Left(error)                    => assert(false, error.toString)
+      }
+    }
+  }
+}
